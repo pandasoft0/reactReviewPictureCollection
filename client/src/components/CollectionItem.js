51ec554a4fe9f142db9b443a9c4165248670c591
pandasoft0 <pandasoft0@gmail.com>
2019-07-31 21:02:55 -0700

    [refactor] setting master template so students can work on it from scratch.  Need review

--- client/src/components/CollectionItem.js
+++ client/src/components/CollectionItem.js
@@ -1,11 +1,5 @@
-import React from 'react';
-
-const CollectionItem = (props) => {
-  return (
-    <div onClick={() => props.changeImage(props.image)}>
-      <img src={props.image} alt=""/>
+/*
+    <div>
+      <img src='https://images.unsplash.com/photo-1506780685701-ba404b9e640d?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=100&ixid=eyJhcHBfaWQiOjF9&ixlib=rb-1.2.1&q=80&w=100' alt=""/>
     </div>
-  )
-};
-
-export default CollectionItem;
+*/
