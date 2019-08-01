abbf40738e29870212644c76f888a5d8d2565e8e
pandasoft0 <pandasoft0@gmail.com>
2019-07-31 20:30:56 -0700

    [prog] functionality to add and change images

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -11,6 +11,9 @@ class App extends React.Component {
       collection: [],
       currentImage: null
     }
+
+    this.changeImage = this.changeImage.bind(this);
+    this.addImage = this.addImage.bind(this);
   }
 
   componentDidMount() {
@@ -18,15 +21,23 @@ class App extends React.Component {
     this.setState({ collection, currentImage: collection[0] });
   }
 
+  changeImage(currentImage) {
+    this.setState({ currentImage });
+  }
+
+  addImage(imageUrl) {
+    this.setState({ collection: [...this.state.collection, imageUrl]})
+  }
+
   render() {
     let { collection, currentImage } = this.state;
 
     return (
       <div id="mainApp">
-        <ImageFetcher options={this.props.options} />
+        <ImageFetcher options={this.props.options} addImage={this.addImage}/>
         <div className="collectionRow">
           {currentImage && <ImageDisplay image={currentImage} />}
-          <Collection collection={collection} />
+          <Collection collection={collection} changeImage={this.changeImage}/>
         </div>
       </div>
     )
