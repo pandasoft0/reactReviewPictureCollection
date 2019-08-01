594aac614123242eb39f4f38bb9280fbdb0cabe8
pandasoft0 <pandasoft0@gmail.com>
2019-07-31 20:35:29 -0700

    [prog] ajax request to get image url

--- client/src/components/ImageFetcher.js
+++ client/src/components/ImageFetcher.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 
 class ImageFetcher extends React.Component {
   constructor(props) {
@@ -6,17 +7,28 @@ class ImageFetcher extends React.Component {
     this.state = {
       options: []
     }
+
+    this.getImage = this.getImage.bind(this);
   }
 
   componentDidMount() {
     this.setState({ options: this.props.options });
   }
 
+  getImage(e) {
+    let { value } = e.target;
+
+    axios
+      .get(`https://source.unsplash.com/100x100/?${value}`)
+      .then(result => this.props.addImage(result.request.responseURL))
+      .catch(err => console.error(err));
+  }
+
   render() {
     return (
       <div className="container">
         {this.state.options.map((option, i) =>
-          <button key={i} value={option}>{option}</button>
+          <button key={i} value={option} onClick={this.getImage}>{option}</button>
         )}
       </div>
     )
